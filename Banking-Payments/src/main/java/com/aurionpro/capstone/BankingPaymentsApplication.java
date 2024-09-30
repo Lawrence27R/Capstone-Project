ba5c4a5e98ca5e44c3e29d76a1dc184569703d55
Lawrence <lawrencerodriques06@gmail.com>
2024-09-30 23:07:55 +0530

    First Commit

---
+++ Banking-Payments/src/main/java/com/aurionpro/capstone/BankingPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.aurionpro.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingPaymentsApplication.class, args);
+	}
+
+}
---
+++ Banking-Payments/src/test/java/com/aurionpro/capstone/BankingPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurionpro.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
