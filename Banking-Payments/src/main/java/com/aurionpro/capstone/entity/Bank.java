9b7c7efb5dd5b5b31e87d379f36fbf3b576fa029
Neha <nehaauti05gmail.com>
2024-10-01 13:43:14 +0530

    Document and Bank Table Entity

--- Banking-Payments/src/main/java/com/aurionpro/capstone/entity/Bank.java
+++ Banking-Payments/src/main/java/com/aurionpro/capstone/entity/Bank.java
@@ -1,5 +1,39 @@
 package com.aurionpro.capstone.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "banks")
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class Bank {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-generate bankId
+    @Column(name = "bankId")
+    private int bankId;
+    
+    @NotBlank(message = "Bank Name is mandatory")
+    @Size(min = 2, max = 100, message = "Bank Name must be between 2 and 100 characters.")
+    @Column(name = "bankName")
+    private String bankName;
+    
+    @NotNull(message = "IFSC number is mandatory")
+//    @Pattern(regexp = "\\d{11}", message = "IFSC number must be exactly 11 digits.")
+    @Column(name = "ifscNo")
+    private String ifscNo;
 
 }
--- Banking-Payments/src/main/java/com/aurionpro/capstone/entity/Document.java
+++ Banking-Payments/src/main/java/com/aurionpro/capstone/entity/Document.java
@@ -1,5 +1,38 @@
 package com.aurionpro.capstone.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "documents")
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class Document {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "documentId")
+    private int documentId;
+    
+    @NotBlank(message = "Document link is mandatory")
+    @Pattern(regexp = "^(https?|ftp)://[^\\s/$.?#].[^\\s]*$", message = "Invalid document link format")
+    @Column(name = "documentLink")
+    private String documentLink;
+    
+    @NotBlank(message = "Document name is mandatory")
+    @Size(min = 2, max = 100, message = "Document name must be between 2 and 100 characters.")
+    @Column(name = "documentName")
+    private String documentName;
 }
