e47103f6f293aa288db138f3d0bf3bba8b351e24
bhakti <bhakti@gmail.com>
2024-10-01 13:25:40 +0530

    Client updated

--- Banking-Payments/src/main/java/com/aurionpro/capstone/entity/Client.java
+++ Banking-Payments/src/main/java/com/aurionpro/capstone/entity/Client.java
@@ -2,9 +2,14 @@ package com.aurionpro.capstone.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -15,20 +20,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Data
 public class Client {
-	@Id
-	@Column(name = "registrationNumber")
-	private int registrationNumber;
 
-	@Column(name = "clientName")
-	@NotBlank(message = "Client Name is mandatory")
-	private String clientName;
+    @Id
+    @Column(name = "registrationNumber")
+    @NotNull(message = "Registration number is mandatory")
+    private Integer registrationNumber;
 
-	@Column(name = "founderName")
-	@NotBlank(message = "Founder Name is mandatory")
-	private String founderName;
+    @Column(name = "clientName")
+    @NotBlank(message = "Client Name is mandatory")
+    private String clientName;
 
-	@Column(name = "status")
-	@NotBlank(message = "Founder Name is mandatory")
-	private ClientStatus status;
+    @Column(name = "status")
+    @NotNull(message = "Status is mandatory")
+    @Enumerated(EnumType.STRING)  // Assuming ClientStatus is an enum
+    private ClientStatus status;
 
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    @NotBlank(message = "State is mandatory")
+    private String state;
+
+    @NotBlank(message = "City is mandatory")
+    private String city;
+
+    @NotNull(message = "Account number is mandatory")
+    @Positive(message = "Account number must be positive")
+    private Long accountNumber;
+
+    @NotBlank(message = "Username is mandatory")
+    private String username;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
 }
