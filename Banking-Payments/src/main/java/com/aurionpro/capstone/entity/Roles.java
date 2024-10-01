2b9fbb8007db50ef8292a998af9da9940d158b47
ShubhamR0804 <shubhamramteke0000@gmail.com>
2024-10-01 13:55:46 +0530

    changes in user and role

---
+++ Banking-Payments/src/main/java/com/aurionpro/capstone/entity/Roles.java
@@ -0,0 +1,5 @@
+package com.aurionpro.capstone.entity;
+
+public enum Roles {
+	ROLE_SUPERADMIN, ROLE_CLIENT
+}
--- Banking-Payments/src/main/java/com/aurionpro/capstone/entity/User.java
+++ Banking-Payments/src/main/java/com/aurionpro/capstone/entity/User.java
@@ -1,5 +1,46 @@
 package com.aurionpro.capstone.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId")
+    private int userId;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$", message = "Password must contain at least one digit, one lowercase letter, and one uppercase letter")
+    @Column(name = "password", nullable = false)
+    private String password;
 }
